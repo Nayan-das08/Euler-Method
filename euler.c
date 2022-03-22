194258d1b38bbd0d21c824e373a5d7d19a7aa82b
Nayan <75008442+Nayan-das08@users.noreply.github.com>
2022-03-22 13:27:25 +0530

    Update euler.c

--- euler.c
+++ euler.c
@@ -5,5 +5,18 @@ int main(void) {
    long long iterations = 5;
 
    float y = 1;
+   float x = y * y;
    
+   float interval = 1.0 / iterations;
+   
+   printf("%.6f, %.6f\n", x, y);
+   
+   for(long i = 0; i < iterations; i++) {
+      y = y + interval * (x * x);
+      printf("%.6f, %.6f\n", x, y);
+      x = x + interval;
+   }
+   
+   iterations++;
+   return 0;
 }
